3351220dc1b26586acfc0ed562f2d84b551d78d4
Sergey_Klapushev <113708824+SergeyKlapushev@users.noreply.github.com>
2023-04-23 13:49:31 +0300

    Code corrected

--- Family tree/src/Main.java
+++ Family tree/src/Main.java
@@ -10,34 +10,5 @@ public class Main {
         
         View view = new Console();
         view.start();        
-
-/*
-        Human petr1 = new Human("Пётр", 1672, 6, 9, new Human("Александр"), new Human("Екатерина"));
-        familyTree.addHuman(petr1);
-
-        Human alexey = new Human("Алексей", 1693, 5, 4, new Human("Пётр"), new Human("Елизавета"));
-        familyTree.addHuman(alexey);
-
-        Human gleb = new Human("Глеб", 1712, 2, 25, new Human("Алексей"), new Human("Кристина"));
-        familyTree.addHuman(gleb);
-
-        Human oleg = new Human("Олег", 1743, 12, 10, new Human("Иван"), new Human("Елена"));
-        familyTree.addHuman(oleg);
-
-        Human vasili = new Human("Василий", 2020, 6, 3, new Human("Сергей"), new Human("Вероника"));
-        familyTree.addHuman(vasili);
-
-
-        // Реализован интерфайс iterable
-        for (Human human : familyTree) {
-            System.out.println(human);
-        }
-
-        // Сортировка по имени
-        service.sortByName();
-
-        // Сортировка по дате рождения (От младшего к старшему)
-        service.sortByDateBorn();
-*/
     }
 }
